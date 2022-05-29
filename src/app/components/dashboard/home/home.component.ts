f8e46ec1f538952ce195b3c906e15dbda19ff637
JorgeMaster <jorge@macdiego.com>
2022-05-29 03:08:06 +0200

    home component

--- src/app/components/dashboard/home/home.component.ts
+++ src/app/components/dashboard/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { RestapiService } from 'src/app/services/restapi.service';
+import { UsersService } from 'src/app/services/users.service';
+import { RegisterComponent } from '../../register/register.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  decodedJWT : any;
+  logUser : String;
+  isLogged : Boolean = false;
+  currentUser : Usuario;
+
+  constructor(private dialogRef: MatDialog, private userService : UsersService, private restapi : RestapiService) { }
 
   ngOnInit(): void {
+    this.decodedJWT = this.restapi.userLogged();
+    console.log(this.decodedJWT);
+    if(this.decodedJWT){
+      this.isLogged = true;
+      this.logUser = this.decodedJWT.sub;
+      this.userService.getUserByUsername(this.logUser).subscribe(data => {
+        this.currentUser = data;
+      });
+    }
+  }
+
+  openRegister(){
+    this.dialogRef.open(RegisterComponent);
   }
 
 }
